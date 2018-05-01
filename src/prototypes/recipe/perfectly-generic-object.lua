d1449f61584fc57e4fe0cb1e93a589ceab6c7699
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2018-05-01 16:23:47 +0200

    Change indentation from spaces to tabs (which are objectively superior).

--- src/prototypes/recipe/perfectly-generic-object.lua
+++ src/prototypes/recipe/perfectly-generic-object.lua
@@ -1,29 +1,29 @@
 data:extend(
 {
-  {
-    type = "recipe",
-    name = "perfectly-generic-object",
-    enabled = true,
-	energy_required = 0.1,
-    ingredients = {},
-    result = "perfectly-generic-object"
-  },
-  {
-    type = "recipe",
-    name = "remove-perfectly-generic-object",
-    enabled = true,
-	energy_required = 0.1,
-    icons = {{icon = "__PerfectlyGenericObject__/graphics/icons/remove-perfectly-generic-object.png"}},
-    icon_size = 32,
-    ingredients =
-    {
-      {"perfectly-generic-object", 1}
-    },
-    result = "perfectly-generic-object",
-	result_count = 0,
-    main_product= "",
-    subgroup = "belt",
-	order = "e[perfectly-generic-object]-b[remove-perfectly-generic-object]",
-  }
+	{
+		type = "recipe",
+		name = "perfectly-generic-object",
+		enabled = true,
+		energy_required = 0.1,
+		ingredients = {},
+		result = "perfectly-generic-object"
+	},
+	{
+		type = "recipe",
+		name = "remove-perfectly-generic-object",
+		enabled = true,
+		energy_required = 0.1,
+		icons = {{icon = "__PerfectlyGenericObject__/graphics/icons/remove-perfectly-generic-object.png"}},
+		icon_size = 32,
+		ingredients =
+		{
+			{"perfectly-generic-object", 1}
+		},
+		result = "perfectly-generic-object",
+		result_count = 0,
+		main_product= "",
+		subgroup = "belt",
+		order = "e[perfectly-generic-object]-b[remove-perfectly-generic-object]",
+	}
 }
 )
