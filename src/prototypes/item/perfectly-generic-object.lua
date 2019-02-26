a40d253d557b1c9d0145286e7836e9ba0216700c
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2019-02-26 19:32:18 +0100

    First version for Factorio 0.17.x.

--- src/prototypes/item/perfectly-generic-object.lua
+++ src/prototypes/item/perfectly-generic-object.lua
@@ -5,7 +5,6 @@ data:extend(
 		name = "perfectly-generic-object",
 		icon = "__PerfectlyGenericObject__/graphics/icons/perfectly-generic-object.png",
 		icon_size = 32,
-		flags = {"goes-to-quickbar"},
 		order = "e[perfectly-generic-object]-a[perfectly-generic-object]",
 		stack_size = 255,
 		subgroup = "belt"
