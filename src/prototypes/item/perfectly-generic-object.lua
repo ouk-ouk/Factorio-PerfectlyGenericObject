0ad321efa035cf65ff52b350091e057816306686
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2020-01-29 23:06:18 +0100

    Added mipmaps to the PGO texture.

--- src/prototypes/item/perfectly-generic-object.lua
+++ src/prototypes/item/perfectly-generic-object.lua
@@ -4,7 +4,7 @@ data:extend(
 		type = "item",
 		name = "perfectly-generic-object",
 		icon = "__PerfectlyGenericObject__/graphics/icons/perfectly-generic-object.png",
-		icon_size = 32,
+		icon_size = 64, icon_mipmaps = 4,
 		order = "e[perfectly-generic-object]-a[perfectly-generic-object]",
 		stack_size = 255,
 		subgroup = "belt"
